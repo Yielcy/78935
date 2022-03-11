d25eecf0876c29f6125d01b1fb31df01eb9facf4
root <jonthan.galio108@gmail.com>
2022-03-11 05:43:00 +0000

    axis2-calculadora

--- axis2-calculadora/calculadora.java
+++ axis2-calculadora/calculadora.java
@@ -1,33 +1,18 @@
 public class calculadora {
 
- public int suma (int a, int b){
+    public int suma (int a, int b){
+        return a+b;
+    }
 
-  return a+b;
+    public int resta (int a, int b){
+        return a-b;
+    }
 
- }
+    public int multiplica (int a, int b){
+        return a*b;
+    }
 
- 
-
- public int resta (int a, int b){
-
-  return a-b;
-
- }
-
- 
-
- public int multiplica (int a, int b){
-
-  return a*b;
-
- }
-
- 
-
- public int divide (int a, int b){
-
-  return a/b;
-
- }
-
+    public int divide (int a, int b){
+        return a/b;
+    }
 }
